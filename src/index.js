93cce68edb9c3b182e185ea0dc7c0f8855275f91
t532 <2037246484@qq.com>
2018-08-26 21:15:28 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import config from '../config.json'
 import Command from './command'
 import chalk from 'chalk'
 import path from 'path'
+import * as commands from './commands'
 import { writeFileSync, existsSync, mkdirSync } from 'fs'
 import { createInterface } from 'readline'
 import { version } from '../package.json'
@@ -9,10 +10,10 @@ import { spawn } from 'child_process'
 const interpreter = new Command({
     prefixes: { options: '-' },
     handlers: {
-        default() { write(chalk.red('Command not found \n')) },
-        invalid() { write(chalk.red('Bad command format \n')) },
+        default() { cli.write(chalk.red('Command not found ')) },
+        invalid() { cli.write(chalk.red('Bad command format ')) },
     }
-}).onAll
+}).onAll(commands)
 const lineInput = createInterface({
     input: process.stdin,
     output: process.stdout,
@@ -20,37 +21,51 @@ const lineInput = createInterface({
 const welcome = `
 nodebot CLI v${version}
 by trustgit, https://gitlab.com/trustgit/nodebot-cli
-
 `
-function write(query) { lineInput.write(query) }
-function question(query) { return new Promise(resolve => lineInput. question(query, answer => resolve(answer))) }
-function command(comm, param) { return new Promise((resolve, reject) => spawn(comm, param).on('close', (err, stdout) => { err ? reject(err) : resolve(stdout) })) }
-async function install() {
-    write(chalk.blue(`Downloading nodebot ... \n`))
+const cli = {
+    write(query) { console.log(query) },
+    question(query) { return new Promise(resolve => lineInput. question(query, answer => resolve(answer))) },
+    exec(comm, param, options = { cwd: undefined, env: process.env }) { return new Promise((resolve, reject) => {
+        const childProcess = spawn(comm, param, options)
+        childProcess.on('close', code => code ? reject(code) : resolve(code))
+    }) },
+}
+async function download() {
+    cli.write(chalk.blue(`Downloading nodebot ... `))
     if (!existsSync(config.path)) mkdirSync(config.path)
-    try { await command('git', ['clone', config.coreRepo, config.path]) }
+    try { await cli.exec('git', ['clone', config.coreRepo, config.path]) }
     catch {
-        write(chalk.red(`Error occured when downloading nodebot. Try deleting everything in ${config.path} and retry. \n`))
+        cli.write(chalk.red(`Error occured when downloading nodebot. Try deleting everything in ${config.path} and retry. `))
         process.exit()
     }
+    await interpreter.do('install', cli, config)
+    cli.write(chalk.blue(`Downloading finished. To start using nodebot, open another shell and type:
+
+    $ cd ${config.path}
+    $ npm start
+    `))
 }
 async function listen() {
-    const command = await question(chalk.gray('nodebot > '))
-    interpreter.do(command)
+    const command = await cli.question(chalk.gray('nodebot > '))
+    await interpreter.do(command, cli, config)
 }
 async function initialize() {
     let installed = true
-    write(welcome)
+    cli.write(welcome)
     if (!config.path) {
-        write(`You haven't specified the path for nodebot. \n`)
-        installed = (await question(`Have you already installed nodebot on your computer? [${chalk.green('Y')}/${chalk.red('(N)')}] > `)).toLowerCase() === 'y' ? true : false
-        config.path = await question(`Specify a path for nodebot > `)
+        cli.write(`You haven't specified the path for nodebot. `)
+        installed = (await cli.question(`Have you already installed nodebot on your computer? [${chalk.green('Y')}/${chalk.red('(N)')}] > `)).toLowerCase() === 'y' ? true : false
+        config.path = await cli.question(`Specify a path for nodebot > `)
         if (!path.isAbsolute(config.path)) config.path = path.join(process.cwd(), config.path)
-        writeFileSync(`${__dirname}/../config.json`, JSON.stringify(config))
     }
+    if (!config.pm) {
+        cli.write(`You haven't specified your package manager. `)
+        config.pm = await cli.question(`Enter your package manager command (npm) > `)
+    }
+    writeFileSync(`${__dirname}/../config.json`, JSON.stringify(config))
     if (!existsSync(`${config.path}/package.json`)) {
-        if (installed) write(chalk.red(`nodebot isn't found in ${config.path}. \n`))
-        await install()
+        if (installed) cli.write(chalk.red(`nodebot isn't found in ${config.path}. `))
+        await download()
     }
 }
 export async function main() { 
